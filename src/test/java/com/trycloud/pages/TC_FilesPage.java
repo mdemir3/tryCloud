ed7e791fc16a68371ef2776dbe8c602d8124e468
tamar <tamaramicov@gmail.com>
2023-03-07 13:20:54 -0500

    US06 feature class was updated US06 Steps Deff is created TC_Files page was Created

---
+++ src/test/java/com/trycloud/pages/TC_FilesPage.java
@@ -0,0 +1,11 @@
+package com.trycloud.pages;
+
+public class TC_FilesPage {
+
+
+
+
+
+
+
+}
---
+++ src/test/java/com/trycloud/step_definitions/US06_StepDeffs.java
@@ -0,0 +1,36 @@
+package com.trycloud.step_definitions;
+
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class US06_StepDeffs {
+
+    @When("the users click action-icon from any file on the page to remove")
+    public void the_users_click_action_icon_from_any_file_on_the_page_to_remove() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("user choose the {string} option")
+    public void user_choose_the_option(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("user click the {string} sub-module on the left side")
+    public void user_click_the_sub_module_on_the_left_side(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("Verify that the file is not listed in the Favorites table")
+    public void verify_that_the_file_is_not_listed_in_the_favorites_table() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+
+
+
+
+}
