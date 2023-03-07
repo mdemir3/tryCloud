dc06f22560d3d508ae4d3b3b034506e2f0e6407e
Hikmet Demir <hikmetdemir2424@gmail.com>
2023-03-07 11:16:04 -0600

    Login Completed 2

--- src/test/java/com/trycloud/pages/FilesHD_Page.java
+++ src/test/java/com/trycloud/pages/FilesHD_Page.java
@@ -3,22 +3,13 @@ package com.trycloud.pages;
 
 
 
-import com.trycloud.utilities.BrowserUtils;
 import com.trycloud.utilities.Driver;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.interactions.Actions;
-import org.openqa.selenium.support.CacheLookup;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
-import java.time.Duration;
-import java.util.List;
-
-public  class BasePage {
-    public BasePage(){
+public  class FilesHD_Page {
+    public FilesHD_Page(){
         PageFactory.initElements(Driver.getDriver(),this);
 
     }
--- src/test/java/com/trycloud/step_definitions/US06_StepDefs.java
+++ src/test/java/com/trycloud/step_definitions/US06_StepDefs.java
@@ -1,18 +1,13 @@
 package com.trycloud.step_definitions;
 
-import com.github.javafaker.Faker;
-import com.trycloud.pages.BasePage;
-import com.trycloud.pages.LoginPage;
-import com.trycloud.utilities.ConfigurationReader;
-import com.trycloud.utilities.Driver;
+import com.trycloud.pages.FilesHD_Page;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import org.junit.Assert;
-import org.openqa.selenium.By;
 
 public class US06_StepDefs {
-    BasePage basePage = new BasePage();
+    FilesHD_Page filesHDPage = new FilesHD_Page();
 
     @Given("user on the dashboard page")
     public void user_on_the_dashboard_page() {
@@ -23,36 +18,36 @@ public class US06_StepDefs {
     @When("the user clicks the {string} module")
     public void the_user_clicks_the_module(String string) {
 
-        basePage.Files.click();
+        filesHDPage.Files.click();
     }
     @When("user clicks the add icon on the top")
     public void user_clicks_the_add_icon_on_the_top() throws InterruptedException {
 
         Thread.sleep(5000);
-      basePage.icon.click();
+      filesHDPage.icon.click();
 
     }
     @When("user click \"new folder”")
     public void user_click_new_folder() {
         // Write code here that turns the phrase above into concrete actions
-        basePage.newFolder.click();
+        filesHDPage.newFolder.click();
 
     }
     @When("user write a folder name")
     public void user_write_a_folder_name() {
         // Write code here that turns the phrase above into concrete actions
 
-        basePage.FolderName.sendKeys("file13");
+        filesHDPage.FolderName.sendKeys("file13");
     }
     @When("the user click submit icon")
     public void the_user_click_submit_icon() {
 
-        basePage.arrow.click();
+        filesHDPage.arrow.click();
 
     }
     @Then("Verify the folder is displayed on the page")
     public void verify_the_folder_is_displayed_on_the_page() {
-        Assert.assertTrue(basePage.fileVerify.isDisplayed());
+        Assert.assertTrue(filesHDPage.fileVerify.isDisplayed());
 
     }
 }
