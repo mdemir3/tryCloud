f7dc6fa735523ee56bcf0a3b38ce05a52859afcb
tamar <tamaramicov@gmail.com>
2023-03-08 11:45:37 -0500

    Configuration was updated with new link

--- src/test/java/com/trycloud/step_definitions/US06_StepDeffs.java
+++ src/test/java/com/trycloud/step_definitions/US06_StepDeffs.java
@@ -15,20 +15,17 @@ public class US06_StepDeffs {
 
     @When("the users click action-icon from any file on the page to remove")
     public void the_users_click_action_icon_from_any_file_on_the_page_to_remove() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+       tc_filesPage.actionIcon.click();
     }
 
     @When("user choose the {string} option")
     public void user_choose_the_option(String string) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+      tc_filesPage.removeFromFavoritesButton.click();
     }
 
     @When("user click the {string} sub-module on the left side")
     public void user_click_the_sub_module_on_the_left_side(String string) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+
     }
 
     @Then("Verify that the file is not listed in the Favorites table")
