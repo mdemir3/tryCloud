5c33628d475ad20e1f8298f15d8cd7347b5ac6cc
tamar <tamaramicov@gmail.com>
2023-03-08 11:43:11 -0500

    TC_Files - locaters were found

--- src/test/java/com/trycloud/pages/TC_FilesPage.java
+++ src/test/java/com/trycloud/pages/TC_FilesPage.java
@@ -1,6 +1,27 @@
 package com.trycloud.pages;
 
+import com.trycloud.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
 public class TC_FilesPage {
+
+    public TC_FilesPage (){
+        PageFactory.initElements(Driver.getDriver(),this);
+    }
+
+    @FindBy (xpath = "(//span[@class='icon icon-more'])[2]")
+    public WebElement actionIcon;
+
+    @FindBy (xpath = "//span[normalize-space()='Remove from favorites']")
+    public WebElement removeFromFavoritesButton;
+
+    @FindBy (xpath = "//a[normalize-space()='Favorites']")
+    public WebElement favoriteSubModule;
+
+    @FindBy (xpath = "//td[@class='filename']//span[@class='innernametext'][normalize-space()='New folder']")
+    public WebElement folderInFavorites;
 
 
 
--- src/test/java/com/trycloud/step_definitions/US06_StepDeffs.java
+++ src/test/java/com/trycloud/step_definitions/US06_StepDeffs.java
@@ -1,9 +1,17 @@
 package com.trycloud.step_definitions;
 
+import com.trycloud.pages.BasePage;
+import com.trycloud.pages.LoginPage;
+import com.trycloud.pages.TC_FilesPage;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
 public class US06_StepDeffs {
+
+    BasePage basePage = new BasePage();
+    LoginPage loginPage = new LoginPage();
+    TC_FilesPage tc_filesPage = new TC_FilesPage();
+
 
     @When("the users click action-icon from any file on the page to remove")
     public void the_users_click_action_icon_from_any_file_on_the_page_to_remove() {
