69eaa2bbd4f2f11933c8dd53bdda0a151979a316
asel <aselko162@yahoo.com>
2023-03-09 10:15:55 -0500

    Asel US

--- src/test/java/com/trycloud/pages/UploadFilePage.java
+++ src/test/java/com/trycloud/pages/UploadFilePage.java
@@ -7,20 +7,23 @@ import org.openqa.selenium.support.PageFactory;
 
 public class UploadFilePage {
     public UploadFilePage(){
+
         PageFactory.initElements(Driver.getDriver(),this);
     }
 
-    @FindBy(css = "[data-id='files']")
+    @FindBy(css = "[data-id= 'files']")
     public WebElement files;
+
     @FindBy(xpath = "/html/body/div[3]/div[2]/div[2]/table/tbody/tr[1]")
     public WebElement NewFolder;
-
 
     @FindBy(xpath = "/html/body/div[3]/div[2]/div[2]/div[1]/div[2]/a")
     public WebElement plus;
 
-    @FindBy(xpath = "/html/body/div[3]/div[2]/div[2]/div[1]/div[2]/div[2]/ul/li[1]/label")
+    @FindBy(xpath = "/html/body/div[3]/div[2]/div[2]/div[1]/div[2]/div[2]/ul/li[1]")
     public WebElement Upload;
+
     @FindBy(xpath = "/html/body/div[3]/div[2]/div[2]/table/tbody/tr")
     public WebElement verifyFile;
+
 }
