bc4e0a8f06fd41b263ae24c4ef33e5818fe6658a
Valerya <ayrelavg@gmail.com>
2023-03-09 12:14:44 -0600

    User story 14 completed

--- src/test/java/com/trycloud/step_definitions/VG_StepDefs.java
+++ src/test/java/com/trycloud/step_definitions/VG_StepDefs.java
@@ -1,16 +1,23 @@
 package com.trycloud.step_definitions;
 
 import com.trycloud.pages.AccessFilesModule;
+import com.trycloud.pages.AccessPhotosModule;
+import com.trycloud.utilities.BrowserUtils;
 import com.trycloud.utilities.Driver;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import org.junit.Assert;
 import org.openqa.selenium.WebElement;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class VG_StepDefs {
 
 
     AccessFilesModule accessFilesModule = new AccessFilesModule();
+    AccessPhotosModule accessPhotosModule = new AccessPhotosModule();
     @Then("verify the page title is \"Files - Trycloud QA.”")
     public void verify_the_page_title_is_files_trycloud_qa() {
 
@@ -40,5 +47,22 @@ public class VG_StepDefs {
     }
 
 
+    @When("the user clicks the Photos module")
+    public void theUserClicksThePhotosModule() {
 
+        accessPhotosModule.photos.click();
+
+    }
+
+    @Then("verify there are following sub-modules")
+    public void verifyThereAreFollowing(List<String> expectedModules) {
+
+        List<String> actual = new ArrayList<>();
+       for(WebElement each : accessPhotosModule.sub_modules){
+          actual.add(each.getText());
+       }
+
+        Assert.assertEquals(actual, expectedModules);
+
+    }
 }
