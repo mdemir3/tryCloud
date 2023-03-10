7a1c6f44a6dc84fabe85f6a68e002a2f8511fe38
Hikmet Demir <hikmetdemir2424@gmail.com>
2023-03-10 11:50:05 -0600

    User Story2 Completed

--- src/test/java/com/trycloud/step_definitions/US09_StepDefs.java
+++ src/test/java/com/trycloud/step_definitions/US09_StepDefs.java
@@ -2,17 +2,19 @@ package com.trycloud.step_definitions;
 
 import com.trycloud.pages.BasePage;
 import com.trycloud.pages.US05Page;
+import com.trycloud.pages.UploadFilePage;
 import com.trycloud.utilities.Driver;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.interactions.Actions;
 
+import java.io.File;
 import java.util.TreeMap;
 
 public class US09_StepDefs {
 
-    BasePage basePage = new BasePage();
+UploadFilePage uploadFilePage = new UploadFilePage();
 
     US05Page us05Page = new US05Page();
 
@@ -21,18 +23,17 @@ public class US09_StepDefs {
     @When("user choose a folder from the page")
     public void user_choose_a_folder_from_the_page() {
 
-        us05Page.FolderOpen.click();
+        uploadFilePage.files.click();
 
     }
 
     @When("the user uploads a file with the upload file option")
     public void the_user_uploads_a_file_with_the_upload_file_option() throws InterruptedException {
 
-        us05Page.uploadIcon.click();
+        String absolutePath = new File("/Users/asel/Desktop/Upload.docx").getAbsolutePath();
+        uploadFilePage.Upload.sendKeys(absolutePath);
         Thread.sleep(3000);
-        Actions actions = new Actions(Driver.getDriver());
-        actions.contextClick(us05Page.upLoadPlus).sendKeys(Keys.ARROW_DOWN)
-                .sendKeys("C:\\Users\\ztpol\\Desktop\\CYDEO\\Interview");
+
     }
 
 
@@ -45,7 +46,7 @@ public class US09_StepDefs {
     @Then("Verify the file is displayed on the page")
     public void verify_the_file_is_displayed_on_the_page() throws InterruptedException {
         Thread.sleep(3000);
-        us05Page.FolderOpen.click();
+        uploadFilePage.verifyFile.isDisplayed();
 
     }
 
