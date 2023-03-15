dd34cfb9016c30c60de59656fe867c438be1a7bf
Hikmet Demir <hikmetdemir2424@gmail.com>
2023-03-15 18:39:30 -0500

    User Story2 Completed

--- src/test/java/com/trycloud/runners/CukesRunner.java
+++ src/test/java/com/trycloud/runners/CukesRunner.java
@@ -14,7 +14,7 @@ import org.junit.runner.RunWith;
         },
         features = "src/test/resources/features",
         glue = "com/trycloud/step_definitions",
-        dryRun = true,
+        dryRun = false,
         tags = "@regression",
         publish = true //generating a report with public link
 
